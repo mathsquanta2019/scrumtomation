448ec8ed9f0035cecffc7fec416e82b2dd2cc7b7
samuel <so.oluwfemi@gmail.com>
2021-02-24 11:45:44 -0600

    enable jpa autiding

---
+++ readme.md
@@ -0,0 +1,3 @@
+This web app automates a simple srcum retrspective process.
+Two users are configured by default: user and sam.Use username as password.
+In memory h2-database name is mathsquanta. The database is accessible at localhost:9091/h2-console with username sa. Leave password field blank.
--- src/main/java/com/safemtech/process/automation/srumtomation/configs/SecurityConfig.java
+++ src/main/java/com/safemtech/process/automation/srumtomation/configs/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -19,6 +20,7 @@ import com.safemtech.process.automation.srumtomation.service.UserService;
 
 @Configuration
 @EnableWebSecurity
+@EnableJpaAuditing
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private UserService userService;
